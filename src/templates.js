04900fe3c81c1628503849e377e54e1a6620f18e
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-03-14 23:34:07 -0300

    create file for the templates📜

---
+++ src/templates.js
@@ -0,0 +1,43 @@
+export const templates = {
+    html5: `
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
+                <title></title>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <link href="css/style.css" rel="stylesheet">
+            </head>
+            <body>
+            
+            </body>
+        </html>
+    `,
+
+    header: {
+        html: `
+            <header>
+                <h1>Hellow World!</h1>
+            </header>
+        `,
+
+        css: `
+            /* styles for gatsby */
+        `
+    },
+
+    social_menu_layers: `
+        <div>
+            <i>Face</i>
+            <i>Face</i>
+            <i>Face</i>
+            <i>Face</i>
+        </div>
+    `,
+
+    loading_effect: `
+        <div>
+            <h4>Seila</h4>
+        </div>
+    `
+};
